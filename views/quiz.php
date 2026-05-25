a07383e69923d88ae76c1cb3e7c1118a3a23765c
KarinaAini <karinaaini354@gmail.com>
2026-05-25 09:38:32 +0700

    Update quiz.php

--- views/quiz.php
+++ views/quiz.php
@@ -107,7 +107,7 @@ shuffle($questions);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz Fakta atau Hoaks</title>
 
-    <link rel="stylesheet" href="../public/css/quiz.css">
+    <link rel="stylesheet" href="../public/css/style.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
