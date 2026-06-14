733a2a67710a847b5726db9a50a6b9466867a4a4
viandraagsatri <vagsatri@gmail.com>
2026-06-14 18:50:19 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$db_host = getenv('DB_HOST') ?: "localhost";
-$db_user = getenv('DB_USER') ?: "root";
-$db_pass = getenv('DB_PASSWORD') ?: "";
-$db_name = getenv('DB_NAME') ?: "si_aksi";
 
-$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$koneksi = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "si_aksi"
+);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
